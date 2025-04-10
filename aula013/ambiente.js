f9330dc4b0ea98946ec194438b40f2b23dd1597c
Pedro Matheus <46729924+pedromatheus-dev@users.noreply.github.com>
2025-04-10 14:31:21 -0300

    criei ambiente aula013

---
+++ aula013/ambiente.js
@@ -0,0 +1,19 @@
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+
+
+var c = 1
+while (c <= 6){
+    console.log(`Tudo bem? - Passo ${c}`)
+    c++
+}
+
+var d = 1
+do{
+    console.log(`Tudo bem? - Passo ${d}`)
+    d++
+} while (d <= 6)
