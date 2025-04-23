6cc11c56249339afafd329825ed407a5974174e9
Pedro Matheus <46729924+pedromatheus-dev@users.noreply.github.com>
2025-04-23 14:29:20 -0300

    criei aula32

---
+++ CFB Cursos/aula32/script.js
@@ -0,0 +1,16 @@
+const c1 = [...document.getElementsByClassName('c1')]
+const c2 = [...document.getElementsByClassName('c2')]
+const colecao = [...document.getElementsByClassName('curso')]
+
+c1.map((e, i) => {
+    e.classList.add('destaque')
+})
+
+c2.map((e, i) => {
+    e.classList.add('destaque')
+})
+
+colecao.map((e, i) => {
+    e.classList.add('destaque')
+})
+
