1caa6171ea6e389bd511a52fb6616b82282e0a18
Pedro Matheus <46729924+pedromatheus-dev@users.noreply.github.com>
2025-04-23 14:29:12 -0300

    criei aula30

---
+++ CFB Cursos/aula30/script.js
@@ -0,0 +1,17 @@
+// GetElementById
+
+const c1 = document.getElementById('c1')
+const c2 = document.getElementById('c2')
+const c3 = document.getElementById('c3')
+const c4 = document.getElementById('c4')
+const c5 = document.getElementById('c5')
+const c6 = document.getElementById('c6')
+
+const arrayC = [
+    c1, c2, c3, c4, c5, c6
+]
+
+arrayC.map((e, i) => {
+    console.log(e.innerHTML)
+    console.log(e)
+})
