1c445f05024e99850eb6aabb8af1b4be31e25b8a
Pedro Matheus <46729924+pedromatheus-dev@users.noreply.github.com>
2025-04-27 22:22:23 -0300

    criei aula34

---
+++ CFB Cursos/aula34/script.js
@@ -0,0 +1,10 @@
+const colecao = [...document.getElementsByClassName('curso')]
+
+colecao.map((el) => {
+    el.addEventListener('click', (evt) => {
+        const elementoClicado = evt.target
+        elementoClicado.classList.add('destaque')
+        console.log(`Elemento clicado: ${elementoClicado.innerHTML}`)
+    })
+})
+
