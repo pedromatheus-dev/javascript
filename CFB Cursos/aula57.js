506017345d34658933e974f70d9c91dd8d04ce32
Pedro Matheus <46729924+pedromatheus-dev@users.noreply.github.com>
2025-05-14 12:12:53 -0300

    criei aula57

---
+++ CFB Cursos/aula57.js
@@ -0,0 +1,15 @@
+// Criando uma classe Aluno
+
+class Pessoa{
+    constructor(nome){
+        this.nome = nome;
+    }
+}
+
+const pessoa1 = new Pessoa("Matheus")
+const pessoa2 = new Pessoa("João")
+const pessoa3 = new Pessoa("Maria")
+
+console.log(pessoa1)
+console.log(pessoa2)
+console.log(pessoa3)
